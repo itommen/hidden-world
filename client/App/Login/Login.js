8933293f3949003d8e451db90807c22970f4ac34
itommen <itommen@gmail.com>
2017-07-08 01:34:42 +0300

    fix(Login): start implementing redux form on login page
    -- currently warning at 'input' , 'meta' isn't known props

--- client/App/Login/Login.js
+++ client/App/Login/Login.js
@@ -1,78 +1,111 @@
 import React from 'react';
-
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import CircularProgress from 'material-ui/CircularProgress';
-import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
 import { Redirect } from 'react-router';
 import { Flex } from 'reflexbox';
+import { reduxForm, Field } from 'redux-form';
 
-export default class LoginForm extends React.Component {
-    constructor() {
-        super();
-        this.updateState = this.updateState.bind(this);
-        this.onLogin = this.onLogin.bind(this);
-    }
+const LoginForm = props => (
+    <Flex column auto justify="center" align="center">
+        <form>
+            <Card>
+                <CardTitle title={'התחבר'} />
+                <CardText>
+                    <Flex column auto>
+                        <Field name={'userName'} component={TextField} floatingLabelText={'שם משתמש'} />
+                        <Field name={'password'} component={TextField} floatingLabelText={'סיסמא'} />
+                    </Flex>
+                </CardText>
+                <CardActions>
+                    {/*<Flex column auto align="center">
+                        <RaisedButton
+                        name={'asd'}
+                            label={'התחבר'}
+                            primary={true}
+                            onClick={() => {
+                                console.log(props);
+                                debugger;
+                            }
+                            } />
+                    </Flex>*/}
+                </CardActions>
+            </Card>
+        </form>
+    </Flex>
+);
 
-    componentDidMount() {
-        this.setState({
-            userName: '',
-            password: ''
-        });
-    }
+export default reduxForm({
+    form: 'loginForm',
+})(LoginForm);
 
-    render() {
-        const { state } = this.props;
-        const loginInProgres = state === 'loginOnProgress';
+// export default class LoginForm extends React.Component {
+//     constructor() {
+//         super();
+//         this.updateState = this.updateState.bind(this);
+//         this.onLogin = this.onLogin.bind(this);
+//     }
 
-        return (
-            <Flex column auto justify="center" align="center">
-                <Card>
-                    <CardTitle title={'התחבר'} />
-                    <CardText>
-                        <Flex column auto>
-                            <TextField
-                                floatingLabelText="שם משתמש"
-                                name="userName"
-                                onChange={this.updateState}
-                                disabled={loginInProgres}
-                            />
+//     componentDidMount() {
+//         this.setState({
+//             userName: '',
+//             password: ''
+//         });
+//     }
 
-                            <TextField
-                                floatingLabelText="סיסמא"
-                                name="password"
-                                onChange={this.updateState}
-                                disabled={loginInProgres}
-                            />
-                        </Flex>
-                    </CardText>
-                    <CardActions>
-                        <Flex column auto align="center">
-                            <RaisedButton
-                                label={loginInProgres ? <CircularProgress size={20} /> : 'התחבר'}
-                                primary={true}
-                                disabled={loginInProgres}
-                                onClick={this.onLogin} />
-                        </Flex>
-                    </CardActions>
-                </Card>
-            </Flex>
-        );
-    }
+//     render() {
+//         const { state } = this.props;
+//         const loginInProgres = state === 'loginOnProgress';
 
-    updateState({ target }) {
-        this.setState(prev => Object.assign(prev, {
-            [target.name]: target.value
-        }));
-    }
+//         return (
+//             <Flex column auto justify="center" align="center">
+//                 <Card>
+//                     <CardTitle title={'התחבר'} />
+//                     <CardText>
+//                         <Flex column auto>
+//                             <TextField
+//                                 floatingLabelText="שם משתמש"
+//                                 name="userName"
+//                                 onChange={this.updateState}
+//                                 disabled={loginInProgres}
+//                             />
 
-    onLogin() {
-        const { login } = this.props;
-        const { userName, password } = this.state;
+//                             <TextField
+//                                 floatingLabelText="סיסמא"
+//                                 name="password"
+//                                 onChange={this.updateState}
+//                                 disabled={loginInProgres}
+//                             />
+//                         </Flex>
+//                     </CardText>
+//                     <CardActions>
+//                         <Flex column auto align="center">
+//                             <RaisedButton
+//                                 label={loginInProgres ? <CircularProgress size={20} /> : 'התחבר'}
+//                                 primary={true}
+//                                 disabled={loginInProgres}
+//                                 onClick={this.onLogin} />
+//                         </Flex>
+//                     </CardActions>
+//                 </Card>
+//             </Flex>
+//         );
+//     }
 
-        login({
-            userName,
-            password
-        });
-    }
-}
+//     updateState({ target }) {
+//         this.setState(prev => Object.assign(prev, {
+//             [target.name]: target.value
+//         }));
+//     }
+
+//     onLogin() {
+//         const { login } = this.props;
+//         const { userName, password } = this.state;
+
+//         login({
+//             userName,
+//             password
+//         });
+//     }
+// }
