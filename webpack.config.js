d5b5535330721395f5c7b826851a663aeac213ea
itommen <itommen@gmail.com>
2017-07-14 17:13:20 +0300

    fix(lint): finish fixing lint
    - fixed vscode lint indention problem when auto format
    - added server lint
    - added dist to .gitignore
    - fixed some last files to pass the lint

--- webpack.config.js
+++ webpack.config.js
@@ -2,68 +2,68 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-	entry: {
-		index: './client/index.js'
-	},
-	output: {
-		path: path.join(__dirname, 'dist/client'),
-		filename: '[name].[hash].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				options: {
-					presets: ['es2015', 'react']
-				},
-			},
-			{
-				test: /\.woff(2)?(\?v=[0-9]+\.[0-9]+\.[0-9]+)?$/,
-				use: [{
-					loader: 'url-loader',
-					options: { limit: 10000 }
-				}]
-			},
-			{
-				test: /\.(ttf|eot|svg)(\?v=[0-9]+\.[0-9]+\.[0-9]+)?$/,
-				use: ['file-loader']
-			},
-			{
-				test: /\.css?$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.less?$/,
-				loader: ['style-loader', 'css-loader', 'less-loader']
-			},
-			{
-				test: /\.html$/,
-				loader: ['html-loader']
-			}]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './client/index.html',
-			filename: 'index.html',
-			inject: 'body'
-		})
-	],
-	resolve: {
-		extensions: ['.js', 'jsx', '.less', '.css', '.html']
-	},
-	devServer: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:1234/',
-				secure: false
-			}
-		},
-		historyApiFallback: {
-			index: 'index.html'
-		}
-	},
-	devtool: 'source-map',
-	watch: true
+  entry: {
+    index: './client/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist/client'),
+    filename: '[name].[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]+\.[0-9]+\.[0-9]+)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: { limit: 10000 }
+        }]
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]+\.[0-9]+\.[0-9]+)?$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less?$/,
+        loader: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: ['html-loader']
+      }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', 'jsx', '.less', '.css', '.html']
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1234/',
+        secure: false
+      }
+    },
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  devtool: 'source-map',
+  watch: true
 };
