b385b1ac994db5aac34a68177d79aefc0ac10c39
itommen <itommen@gmail.com>
2018-04-01 17:17:12 -0800

    fix(left-list-item-action): not the action is clickable

--- client/App/common/LeftListItemSecondaryAction/index.js
+++ client/App/common/LeftListItemSecondaryAction/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import { ListItemSecondaryAction } from 'material-ui/List';
 
-export default ({ children }) => <ListItemSecondaryAction style={{
-  right: 'unset',
-  left: '4px'
-}}>
-  {children}
-</ListItemSecondaryAction>
+export default class LeftListItemSecondaryAction extends ListItemSecondaryAction {
+  render() {
+    const { children } = this.props;
+    return <ListItemSecondaryAction style={{
+      right: 'unset',
+      left: '4px'
+    }}>
+      {children}
+    </ListItemSecondaryAction>
+  }
+}
